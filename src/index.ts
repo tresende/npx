b29849b9a4b7f62a0732f09e654f08960d2205b7
Thiago Resende <thiago.gcresende@gmail.com>
2024-05-02 20:19:30 -0300

    build: update build script to include shebang in generated file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Click here 👉 https://thiagoresende.dev')
+const message = 'Click here 👉 https://thiagoresende.dev'
+const linkedin = 'https://www.linkedin.com/in/thiago-resende-46175391/'
+const github = 'https://github.com/tresende'
+
+const output = `  \x1b[38;5;51m+---------------------------------------------------------------+
+  |   \x1b[38;5;105m${message}\x1b[38;5;51m                     |
+  +---------------------------------------------------------------+
+  | \x1b[38;5;93mLinkedIn:\x1b[0m \x1b[38;5;39m${linkedin}\x1b[38;5;51m|
+  | \x1b[38;5;93mGithub:\x1b[0m  \x1b[38;5;39m${github}                          \x1b[38;5;51m|
+  +---------------------------------------------------------------+\x1b[0m`
+
+console.log(output)
